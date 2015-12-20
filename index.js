a6a899f2827ebfb4b67734a025fc676be0268630
Loren Norman <lorennorman@gmail.com>
2015-12-19 23:36:13 -0500

    better messaging from Node on Lambda

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ var spawn = require('child_process').spawn;
 var invokeRubyApp = "./app";
 
 exports.handler = function(event, context) {
+  console.log("Starting process: " + invokeRubyApp);
   var child = spawn(invokeRubyApp);
 
   child.stdout.on('data', function (data) { console.log("stdout:\n"+data); });
   child.stderr.on('data', function (data) { console.log("stderr:\n"+data); });
 
-  child.on('close', function (code) { context.done(); });
+  child.on('close', function (code) {
+    if(code === 0) {
+      context.succeed("Process completed: " + invokeRubyApp);
+    } else {
+      context.fail("Process \"" + invokeRubyApp + "\" exited with code: " + code);
+    }
+  });
 }
